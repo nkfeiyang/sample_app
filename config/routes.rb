49b6aef6ddf4961d967335a0d469bbc52502ec79
unknown <Robin@.(none)>
2010-07-22 15:42:12 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.home    '/home',    :controller => 'pages', :action => 'home'
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
+  
+  map.signup  '/signup',  :controller => 'users', :action => 'new'
   
   # The priority is based upon order of creation: first created -> highest priority.
 
