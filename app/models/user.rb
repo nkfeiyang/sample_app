20954701ce15fdad1f3d832c6f956c335187e189
nkfeiyang <wang_haibing@126.com>
2010-07-23 23:07:26 +0800

    add

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,16 @@
 #  updated_at :datetime
 #
 
+require 'digest'
+
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+  
+  validates_confirmation_of :password
+  
+  validates_presence_of :password
+  validates_length_of   :password, :within => 6..40
   
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -19,4 +27,70 @@ class User < ActiveRecord::Base
   validates_length_of       :name,  :maximum => 50 
   validates_format_of       :email, :with => EmailRegex
   validates_uniqueness_of   :email, :case_sensitive => false
+  
+  before_save  :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypt_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+    
+    def encrypt_password
+      self.salt = make_salt
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      secure_hash("#{salt}#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}#{password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
+  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,7 +15,12 @@ require 'spec_helper'
 describe User do
   
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { 
+      :name => "Example User", 
+      :email => "user@example.com",
+      :password => "foobar",
+      :password_confirmation => "foobar"
+    }
   end
 
   it "should create a new instance given valid attributes" do
@@ -66,6 +71,76 @@ describe User do
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
+  
+  describe "password validateions" do  
+ 
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+        should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+        should_not be_valid
+    end
+    
+    it "should reject short password" do
+      short = "a" * 5
+      User.new(@attr.merge(:password => short, :password_confirmation => short)).
+        should_not be_valid
+    end
+    
+    it "should reject long password" do
+      short = "a" * 41
+      User.new(@attr.merge(:password => short, :password_confirmation => short)).
+        should_not be_valid
+    end
+  end
+  
+  describe "password validateions" do  
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+    
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    describe "has_apssword? method" do
+      
+      it "should be true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should be false if the passwords don't match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+    
+    describe "authenticate method" do
+      
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@attr[:email], "wringpass")
+        wrong_password_user.should be_nil
+      end
+      
+      it "should return nil for an email address with no user" do
+        nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+        nonexistent_user.should be_nil
+      end
+      
+      it "should return the user on email/password match" do
+        matching_user = User.authenticate(@attr[:email], @attr[:password])
+        matching_user.should == @user
+      end
+    end
+    
+  end 
+  
 end
 
 
